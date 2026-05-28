1878f1f3e32b823a4904a57d877f8f093f99fe29
Mohamed0Farouk <mohamed.farouk.dev@gmail.com>
2026-05-28 18:08:23 +0300

    Implement missing isRecordingSoftwareRunning and getDetectedRecordingApp Swift methods

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -19,14 +19,62 @@ class MainFlutterWindow: NSWindow {
     channel.setMethodCallHandler { call, result in
       switch call.method {
       case "isRecordingDetected":
-        result(isRecordingSoftwareRunning())
+        result(self.isRecordingSoftwareRunning())
       case "getDetectedApp":
-        result(getDetectedRecordingApp())
+        result(self.getDetectedRecordingApp())
       default:
         result(FlutterMethodNotImplemented)
       }
     }
 
     super.awakeFromNib()
+  }
+
+  private func isRecordingSoftwareRunning() -> Bool {
+    let suspiciousApps = [
+      "OBS",
+      "QuickTime Player",
+      "ScreenFlow",
+      "Camtasia",
+      "Loom",
+    ]
+
+    let runningApps = NSWorkspace.shared.runningApplications
+
+    for app in runningApps {
+      if let name = app.localizedName {
+        if suspiciousApps.contains(where: {
+          name.localizedCaseInsensitiveContains($0)
+        }) {
+          return true
+        }
+      }
+    }
+
+    return false
+  }
+
+  private func getDetectedRecordingApp() -> String {
+    let suspiciousApps = [
+      "OBS",
+      "QuickTime Player",
+      "ScreenFlow",
+      "Camtasia",
+      "Loom",
+    ]
+
+    let runningApps = NSWorkspace.shared.runningApplications
+
+    for app in runningApps {
+      if let name = app.localizedName {
+        if suspiciousApps.contains(where: {
+          name.localizedCaseInsensitiveContains($0)
+        }) {
+          return name
+        }
+      }
+    }
+
+    return "None"
   }
 }
